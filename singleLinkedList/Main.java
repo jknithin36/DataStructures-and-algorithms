18b98bf2cfd1fb042c1c367ed80d01b0fd6d692e
jknithin36 <89259785+jknithin36@users.noreply.github.com>
2022-09-10 19:58:17 +0530

    Add files via upload

---
+++ singleLinkedList/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+  public static void main(String[]args){
+        singleLinkedList sll = new singleLinkedList();
+        //To create Single Linked List
+        sll.createSingleLinkedList(1);
+        //To insert Values in Single Linked List
+       sll.insertInLinkedList(2, 1);
+       sll.insertInLinkedList(3, 2);
+       sll.insertInLinkedList(4, 3);
+       sll.insertInLinkedList(5, 4);
+       sll.insertInLinkedList(6, 5);
+       //To traverse Single linked list
+       sll.traverseSingleLinkedList();
+       //Searching a node in Single Linked List
+       //sll.searchNode(nodeValue);
+       //To delete Node From Single Linked List
+       //sll.deletionOfNode(location);
+       //To delete Entire Single Linked List
+       //sll.deleteSingleLinkedList();
+       
+    }
+}
