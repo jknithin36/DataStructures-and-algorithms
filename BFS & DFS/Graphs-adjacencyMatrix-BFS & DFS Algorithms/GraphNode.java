d8b10129e90695dca0e701b828731f4e949f1e1e
jknithin36 <89259785+jknithin36@users.noreply.github.com>
2022-09-22 08:48:53 +0530

    Add files via upload

---
+++ BFS & DFS/Graphs-adjacencyMatrix-BFS & DFS Algorithms/GraphNode.java
@@ -0,0 +1,9 @@
+public class GraphNode {
+  public String name;
+  public int index;
+  public boolean isVisited = false;
+  public GraphNode(String name,int index){
+    this.name = name;
+    this.index =index;
+  }
+}
