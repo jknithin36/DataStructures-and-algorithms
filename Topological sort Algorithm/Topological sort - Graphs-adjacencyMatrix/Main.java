5ea2e6ca1da4a5ed149239f48db5d6cefd65bbab
jknithin36 <89259785+jknithin36@users.noreply.github.com>
2022-09-22 10:46:31 +0530

    Add files via upload

---
+++ Topological sort Algorithm/Topological sort - Graphs-adjacencyMatrix/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+class Main{
+  public static void main(String[]args){
+  ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+   nodeList.add(new GraphNode("A",0));
+   nodeList.add(new GraphNode("B",1));
+   nodeList.add(new GraphNode("C",2));
+   nodeList.add(new GraphNode("D",3));
+   nodeList.add(new GraphNode("E",4));
+    nodeList.add(new GraphNode("F",5));
+     nodeList.add(new GraphNode("G",6));
+       nodeList.add(new GraphNode("H",7));
+
+   Graph g = new Graph(nodeList);
+   g.addDirectedEdge(0, 2);
+   g.addDirectedEdge(2, 4);
+   g.addDirectedEdge(4, 7);
+   g.addDirectedEdge(4, 5);
+   g.addDirectedEdge(5, 6);
+    g.addDirectedEdge(1, 2);
+     g.addDirectedEdge(1, 3);
+      g.addDirectedEdge(3, 5);
+
+   System.out.println(g.toString());
+g.topologicalSort();
+
+
+
+
+  }
+}
---
+++ Topological sort Algorithm/Topological sort -Graphs-adjacencyList/Graph.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.LinkedList;
+public class Graph {
+  
+  ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+  public Graph(ArrayList<GraphNode> nodeList){
+    this.nodeList = nodeList;
+  }
+
+  public void addUndirectedEdge(int i , int j){
+    GraphNode frist = nodeList.get(i);
+    GraphNode second = nodeList.get(j);
+    frist.neighbors.add(second);
+    second.neighbors.add(frist);
+  }
+// pring Graph 
+   public String toString() {
+    StringBuilder s = new StringBuilder();
+    for (int i = 0; i < nodeList.size(); i++) {
+      s.append(nodeList.get(i).name + ": ");
+      for (int j =0; j < nodeList.get(i).neighbors.size(); j++) {
+        if (j == nodeList.get(i).neighbors.size()-1 ) {
+          s.append((nodeList.get(i).neighbors.get(j).name) );
+        } else {
+          s.append((nodeList.get(i).neighbors.get(j).name) + " -> ");
+        }
+      }
+      s.append("\n");
+    }
+    return s.toString();
+  }
+  //BFS Traversal
+  void bfsVist(GraphNode node){
+    LinkedList<GraphNode> queue = new LinkedList<GraphNode>();
+    queue.add(node);
+    while(!queue.isEmpty()){
+      GraphNode currentNode = queue.remove(0);
+      currentNode.isVisted=true;
+      System.out.print(currentNode.name+" ");
+      for(GraphNode neighbor: currentNode.neighbors){
+        if(!neighbor.isVisted){
+          queue.add(neighbor);
+          neighbor.isVisted=true;
+        }
+      }
+    }
+  }
+   void bfs(){
+    for(GraphNode node : nodeList){
+      if(!node.isVisted){
+        bfsVist(node);
+      }
+    }
+   }
+//DFS Traversal
+
+void dfsVisit(GraphNode node){
+  Stack<GraphNode> stack = new Stack<>();
+  stack.push(node);
+  while(!stack.isEmpty()){
+  GraphNode currentNode = stack.pop();
+  currentNode.isVisted=true;
+  System.out.print(currentNode.name+" ");
+  for(GraphNode neighbor : currentNode.neighbors){
+    if(!neighbor.isVisted){
+      stack.push(neighbor);
+      neighbor.isVisted=true;
+    }
+  }
+  }
+}
+
+void dfs(){
+  for(GraphNode node: nodeList){
+    if(!node.isVisted){
+      dfsVisit(node);
+    }
+  }
+}
+
+
+
+  public void addDirectedEdge(int i ,int j ){
+    GraphNode frist = nodeList.get(i);
+    GraphNode second = nodeList.get(j);
+    frist.neighbors.add(second);
+  }
+
+  void topologicalVisit(GraphNode node, Stack<GraphNode> stack){
+    for(GraphNode neighbor : node.neighbors){
+      if(!neighbor.isVisted){
+        topologicalVisit(neighbor, stack);
+      }
+    }
+    node.isVisted = true;
+    stack.push(node);
+  }
+
+  void topologicalSort(){
+    Stack<GraphNode> stack = new Stack<>();
+    for(GraphNode node : nodeList){
+      if(!node.isVisted){
+        topologicalVisit(node, stack);
+      }
+    }
+    while(!stack.isEmpty()){
+      System.out.print(stack.pop().name+" ");
+    }
+  }
+}
